cd4499ad9727111cc80971e8e4d49258563c5ad7
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2020-03-05 11:45:35 +0900

    本番環境での表示確認のためspotモデル、コントローラ作成

---
+++ db/migrate/20200305024014_create_spots.rb
@@ -0,0 +1,10 @@
+class CreateSpots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spots do |t|
+
+      t.string :name
+      t.text :address
+      t.timestamps
+    end
+  end
+end
